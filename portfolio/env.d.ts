1b6d106fd08f04323b8f392ef565b4b207fdadf9
Estremadura, Ric Michael <ricmichaele@gmail.com>
2025-06-04 22:24:58 +0800

    bug fixes

--- portfolio/env.d.ts
+++ portfolio/env.d.ts
@@ -1,11 +1,3 @@
 // src/env.d.ts
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
-  readonly VITE_APP_TITLE: string; // Example: You can add your own custom env variables here
-  // Add other Vite-specific env variables if needed, e.g., VITE_API_URL
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
